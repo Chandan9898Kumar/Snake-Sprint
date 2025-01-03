0ed363acc57eac79e5f89e1dd8480e003fac600c
Chandan <Chandan17ee@gmail.com>
2025-01-03 20:15:08 +0530

    chore: used class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Swipe from "react-easy-swipe";
+import "./App.css";
+import React, { Component } from "react";
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.foodClasses = [
+      "blueberry",
+      "cherry",
+      "grapes",
+      "raspberry",
+      "strawberry",
+    ];
+    this.turningPoints = [];
+    this.boardWidth = 20;
+    this.boardHeight = 15;
+    this.snakeDelay = 100;
+    this.allowSwipe = true;
+    this.mode = "easy";
+    this.eatAudio = new Audio("https://github.com/diozz/snake-react-js/raw/main/src/sounds/eat.mp3");
+    this.gameOverAudio = new Audio("https://github.com/diozz/snake-react-js/raw/main/src/sounds/game-over.mp3");
+
+  
+  }
+
+  render() {
+    return (
+      <div className="app-bg">
+        <Swipe
+        
+        className="full-height flex flex-center"
+        >Snake</Swipe>
+      </div>
+    );
+  }
 }
 
 export default App;
