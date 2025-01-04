9c39d655004c9f724bb2816b558afd9945e88f76
Chandan <Chandan17ee@gmail.com>
2025-01-04 15:52:21 +0530

    chore: added game play and over menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Swipe from "react-easy-swipe";
 import "./App.css";
-import './utils.css'
+import "./utils.css";
 import React, { Component } from "react";
 
 import ScoreBoard from "./components/ScoreBoard/ScoreBoard";
@@ -48,7 +48,6 @@ class App extends Component {
       >
         <Swipe className="full-height flex flex-center">
           <div className="container">
-
             <div className="flex">
               <div className="titleContainer">
                 <div className="border">
@@ -65,7 +64,51 @@ class App extends Component {
               </div>
             </div>
 
+            <div className="border mt-2">Game Board</div>
 
+            {this.state.gameMenuDisplay && (
+              <div className="border menu-overlay mt-2">
+                <div className="flex flex-center">
+                  <div className="snake-food snake menu-food mr-4"></div>
+                  <div className="game-over-text">MENU</div>
+                  <div className="snake-food snake invert menu-food ml-4"></div>
+                </div>
+                <div className="flex mt-2">
+                  <span
+                    // onClick={this.startGame.bind(this, 200)}
+                    className="snakeButton mr-1"
+                  >
+                    EASY
+                  </span>
+                  <span
+                    // onClick={this.startGame.bind(this, 120)}
+                    className="snakeButton mr-1 ml-1"
+                  >
+                    MEDIUM
+                  </span>
+                  <span
+                    // onClick={this.startGame.bind(this, 75)}
+                    className="snakeButton ml-1"
+                  >
+                    HARD
+                  </span>
+                </div>
+              </div>
+            )}
+
+            {this.state.gameOverDisplay && (
+              <div className="border gameover-overlay mt-2">
+                <div className="game-over-text">{this.state.gameOverMsg}</div>
+                <div className="flex mt-2">
+                  <span
+                    // onClick={this.endGame}
+                    className="snakeButton mr-1 ml-1"
+                  >
+                    RESTART
+                  </span>
+                </div>
+              </div>
+            )}
           </div>
         </Swipe>
       </div>
