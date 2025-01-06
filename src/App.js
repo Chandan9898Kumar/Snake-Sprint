63459256cf87bfad44c7dde5fc3828419fd5af1e
Chandan <Chandan17ee@gmail.com>
2025-01-06 22:58:07 +0530

    chore: integrated all components to main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
 import Swipe from "react-easy-swipe";
-import "./App.css";
-import "./utils.css";
-
+import { Body, Snake } from "./classes/LinkedList";
+import { TurningPoint } from "./classes/Queue";
 import GameBoard from "./components/GameBoard/GameBoard";
 import ScoreBoard from "./components/ScoreBoard/ScoreBoard";
 import SnakeHeader from "./components/SnakeHeader/SnakeHeader";
+
+import React, { Component } from "react";
+
+import "./App.css";
+import "./utils.css";
+
 class App extends Component {
   constructor() {
     super();
@@ -31,29 +35,372 @@ class App extends Component {
       "https://github.com/diozz/snake-react-js/raw/main/src/sounds/game-over.mp3"
     );
 
-    // let snake = new Snake();
+    let snake = new Snake();
 
-    // snake.add(new Body([1, 3], "R"));
-    // snake.add(new Body([1, 2], "R"));
-    // snake.add(new Body([1, 1], "R"));
+    snake.add(new Body([1, 3], "R"));
+    snake.add(new Body([1, 2], "R"));
+    snake.add(new Body([1, 1], "R"));
 
     this.state = {
       currentScore: 0,
       bestScore: "--",
       gameOverMsg: "",
-      // snake: snake,
+      snake: snake,
       foodCoordinate: [8, 18],
-      // foodClass: this.foodClasses[this.getRandomInt(0, this.foodClasses.length - 1)],
+      foodClass:
+        this.foodClasses[this.getRandomInt(0, this.foodClasses.length - 1)],
       gameOverDisplay: false,
       gameMenuDisplay: true,
     };
 
-    // this.updateSnakeState = this.updateSnakeState.bind(this);
-    // this.onSwipeMove = this.onSwipeMove.bind(this);
-    // this.keyListner = this.keyListner.bind(this);
-    // this.startGame = this.startGame.bind(this);
-    // this.gameOver = this.gameOver.bind(this);
-    // this.endGame = this.endGame.bind(this);
+    this.updateSnakeState = this.updateSnakeState.bind(this);
+    this.onSwipeMove = this.onSwipeMove.bind(this);
+    this.keyListener = this.keyListener.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.gameOver = this.gameOver.bind(this);
+    this.endGame = this.endGame.bind(this);
+  }
+
+  componentDidMount() {
+    this.eatAudio.load();
+    this.gameOverAudio.load();
+
+    document.addEventListener("keydown", this.keyListener, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyListener, false);
+  }
+
+  keyListener(event) {
+    if (this.state.gameMenuDisplay || this.state.gameOverDisplay) return;
+
+    let keyCode = event.keyCode;
+    let tempSnake = this.state.snake;
+
+    let turningPoint = new TurningPoint(null, null);
+
+    switch (keyCode) {
+      case 38:
+        if (
+          tempSnake.head.direction === "T" ||
+          tempSnake.head.direction === "B"
+        )
+          return;
+
+        turningPoint.nextDirection = "T";
+        turningPoint.coordinates = [...tempSnake.head.coordinates];
+        this.turningPoints.push(turningPoint);
+
+        this.setState({ snake: tempSnake });
+        break;
+
+      case 39:
+        if (
+          tempSnake.head.direction === "R" ||
+          tempSnake.head.direction === "L"
+        )
+          return;
+
+        turningPoint.nextDirection = "R";
+        turningPoint.coordinates = [...tempSnake.head.coordinates];
+        this.turningPoints.push(turningPoint);
+
+        this.setState({ snake: tempSnake });
+        break;
+
+      case 40:
+        if (
+          tempSnake.head.direction === "B" ||
+          tempSnake.head.direction === "T"
+        )
+          return;
+
+        turningPoint.nextDirection = "B";
+        turningPoint.coordinates = [...tempSnake.head.coordinates];
+        this.turningPoints.push(turningPoint);
+
+        this.setState({ snake: tempSnake });
+        break;
+
+      case 37:
+        if (
+          tempSnake.head.direction === "L" ||
+          tempSnake.head.direction === "R"
+        )
+          return;
+
+        turningPoint.nextDirection = "L";
+        turningPoint.coordinates = [...tempSnake.head.coordinates];
+        this.turningPoints.push(turningPoint);
+
+        this.setState({ snake: tempSnake });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  startGame(snakeDelay) {
+    switch (snakeDelay) {
+      case 75:
+        this.mode = "hard";
+        break;
+      case 120:
+        this.mode = "medium";
+        break;
+      case 200:
+        this.mode = "easy";
+        break;
+
+      default:
+        return snakeDelay;
+    }
+
+    this.snakeDelay = snakeDelay;
+    this.setState({
+      gameMenuDisplay: false,
+      gameOverDisplay: false,
+      bestScore: this.getBestScore(this.mode),
+    });
+
+    this.intervalId = window.setInterval(
+      this.updateSnakeState,
+      this.snakeDelay
+    );
+  }
+
+  endGame() {
+    let snake = new Snake();
+
+    snake.add(new Body([1, 3], "R"));
+    snake.add(new Body([1, 2], "R"));
+    snake.add(new Body([1, 1], "R"));
+
+    this.setState({
+      currentScore: 0,
+      snake: snake,
+      bestScore: "--",
+      foodCoordinate: [8, 18],
+      foodClass:
+        this.foodClasses[this.getRandomInt(0, this.foodClasses.length - 1)],
+      gameOverDisplay: false,
+      gameMenuDisplay: true,
+    });
+  }
+
+  updateSnakeState() {
+    let tempSnake = this.state.snake;
+    let currentBodyPiece = tempSnake.head;
+
+    if (tempSnake.isOn(currentBodyPiece.coordinates, true)) {
+      this.gameOver();
+      return;
+    }
+
+    //gameOverDetection
+    switch (currentBodyPiece.direction) {
+      case "T":
+        if (currentBodyPiece.coordinates[0] < 0) {
+          this.gameOver();
+          return;
+        }
+        break;
+
+      case "R":
+        if (currentBodyPiece.coordinates[1] > this.boardWidth - 1) {
+          this.gameOver();
+          return;
+        }
+        break;
+
+      case "B":
+        if (currentBodyPiece.coordinates[0] > this.boardHeight - 1) {
+          this.gameOver();
+          return;
+        }
+        break;
+
+      case "L":
+        if (currentBodyPiece.coordinates[1] < 0) {
+          this.gameOver();
+          return;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    //updateSnakePosition
+    while (currentBodyPiece) {
+      //  This below code is having some warnings : Function declared in a loop contains unsafe references to variable(s) 'currentBodyPiece', 'currentBodyPiece'.
+
+      //                  Solution:
+      // The warning you're encountering about "unsafe references" typically arises when a variable is captured in a closure within a loop,
+      // which can lead to unexpected behavior if the variable changes during the loop's execution. In your case, currentBodyPiece is being referenced inside the some
+      // and filter methods, which could lead to issues if currentBodyPiece is modified in subsequent iterations of the loop.
+
+      // if (
+      //   this.turningPoints.some(
+      //     (x) =>
+      //       x.coordinates[0] === currentBodyPiece.coordinates[0] &&
+      //       x.coordinates[1] === currentBodyPiece.coordinates[1]
+      //   )
+      // ) {
+      //   currentBodyPiece.direction = this.turningPoints.filter(
+      //     (x) =>
+      //       x.coordinates[0] === currentBodyPiece.coordinates[0] &&
+      //       x.coordinates[1] === currentBodyPiece.coordinates[1]
+      //   )[0].nextDirection;
+
+      //   if (currentBodyPiece.tail) this.turningPoints.shift();
+      // }
+
+      // To resolve this, you can create a copy of currentBodyPiece or use a different approach to ensure that the reference remains stable throughout
+      // the execution of the loop. Here’s a refactored version of your code that avoids unsafe references:
+
+      /**
+       * Changes Made:
+1. Storing Coordinates: I stored currentBodyPiece.coordinates in a separate variable currentCoordinates to avoid referencing currentBodyPiece directly in the loop.
+2. Using find Instead of filter: I replaced the filter method with find, which is more appropriate when you only need the first matching element. This avoids creating an array unnecessarily.
+3. Check for Existence: Added a check to ensure that turningPoint exists before trying to access its properties.
+       */
+      const currentCoordinates = currentBodyPiece.coordinates;
+
+      if (
+        this.turningPoints.some(
+          (x) =>
+            x.coordinates[0] === currentCoordinates[0] &&
+            x.coordinates[1] === currentCoordinates[1]
+        )
+      ) {
+        const turningPoint = this.turningPoints.find(
+          (x) =>
+            x.coordinates[0] === currentCoordinates[0] &&
+            x.coordinates[1] === currentCoordinates[1]
+        );
+
+        if (turningPoint) {
+          currentBodyPiece.direction = turningPoint.nextDirection;
+
+          if (currentBodyPiece.tail) {
+            this.turningPoints.shift();
+          }
+        }
+      }
+
+      switch (currentBodyPiece.direction) {
+        case "T":
+          currentBodyPiece.coordinates[0]--;
+          break;
+
+        case "R":
+          currentBodyPiece.coordinates[1]++;
+          break;
+
+        case "B":
+          currentBodyPiece.coordinates[0]++;
+          break;
+
+        case "L":
+          currentBodyPiece.coordinates[1]--;
+          break;
+
+        default:
+          break;
+      }
+
+      currentBodyPiece = currentBodyPiece.next;
+    }
+
+    //Ate.
+    if (
+      tempSnake.head.coordinates[0] === this.state.foodCoordinate[0] &&
+      tempSnake.head.coordinates[1] === this.state.foodCoordinate[1]
+    ) {
+      let newScore = this.state.currentScore + 5;
+      let newFoodX = 0;
+      let newFoodY = 0;
+
+      while (true) {
+        newFoodY = this.getRandomInt(0, this.boardHeight - 1);
+        newFoodX = this.getRandomInt(0, this.boardWidth - 1);
+
+        if (
+          newFoodY === this.state.foodCoordinate[0] &&
+          newFoodX === this.state.foodCoordinate[1]
+        )
+          continue;
+
+        if (tempSnake.isOn([newFoodY, newFoodX])) continue;
+
+        break;
+      }
+
+      tempSnake.eat();
+      this.eatAudio.play();
+
+      this.setState({
+        snake: tempSnake,
+        currentScore: newScore,
+        foodCoordinate: [newFoodY, newFoodX],
+        foodClass:
+          this.foodClasses[this.getRandomInt(0, this.foodClasses.length - 1)],
+      });
+    } else {
+      this.setState({ snake: tempSnake });
+    }
+  }
+
+  gameOver() {
+    this.gameOverAudio.play();
+
+    clearInterval(this.intervalId);
+    this.turningPoints = [];
+    let gameOverMsg = "GAME OVER!";
+    let bestScore = this.getBestScore(this.mode);
+    if (this.state.currentScore > bestScore) {
+      localStorage.setItem(this.mode + "BestScore", this.state.currentScore);
+      bestScore = this.state.currentScore;
+      gameOverMsg = "NEW BEST!";
+    }
+
+    this.setState({
+      gameOverDisplay: true,
+      bestScore: bestScore,
+      gameOverMsg: gameOverMsg,
+    });
+  }
+
+  getBestScore(mode) {
+    let bestScore = localStorage.getItem(mode + "BestScore");
+    bestScore = bestScore === null ? 0 : parseInt(bestScore);
+    return bestScore;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  onSwipeMove(position, event) {
+    if (this.state.gameMenuDisplay || this.state.gameOverDisplay) return;
+
+    let tolerance = 2;
+    let x = position.x;
+    let y = position.y;
+
+    if (this.allowSwipe) {
+      if (Math.abs(x) > tolerance || Math.abs(y) > tolerance) {
+        this.allowSwipe = false;
+        if (Math.abs(y) > Math.abs(x))
+          this.keyListener({ keyCode: y > 0 ? 40 : 38 });
+        else this.keyListener({ keyCode: x > 0 ? 39 : 37 });
+      }
+    }
   }
 
   render() {
@@ -61,11 +408,13 @@ class App extends Component {
       <div
         className="app-bg"
         onTouchStart={() => {
-          console.log("touch start");
           this.allowSwipe = true;
         }}
       >
-        <Swipe className="full-height flex flex-center">
+        <Swipe
+          className="full-height flex flex-center"
+          onSwipeMove={this.onSwipeMove}
+        >
           <div className="container">
             <div className="flex">
               <div className="titleContainer">
@@ -83,19 +432,18 @@ class App extends Component {
               </div>
             </div>
 
-            {!this.state.gameOverDisplay && !this.state.gameMenuDisplay && (
-              <div className="border mt-2">
-                <GameBoard
-                  boardWidth={this.boardWidth}
-                  boardHeight={this.boardHeight}
-                  snake={this.state.snake}
-                  currentScore={this.state.currentScore}
-                  foodCoordinate={this.state.foodCoordinate}
-                  foodClass={this.state.foodClass}
-                  gameOverDisplay={this.state.gameOverDisplay}
-                />
-              </div>
-            )}
+            <div className="border mt-2">
+              <GameBoard
+                getRandomInt={this.getRandomInt}
+                boardWidth={this.boardWidth}
+                boardHeight={this.boardHeight}
+                snake={this.state.snake}
+                currentScore={this.state.currentScore}
+                foodCoordinate={this.state.foodCoordinate}
+                foodClass={this.state.foodClass}
+                gameOverDisplay={this.state.gameOverDisplay}
+              />
+            </div>
 
             {this.state.gameMenuDisplay && (
               <div className="border menu-overlay mt-2">
@@ -106,19 +454,19 @@ class App extends Component {
                 </div>
                 <div className="flex mt-2">
                   <span
-                    // onClick={this.startGame.bind(this, 200)}
+                    onClick={this.startGame.bind(this, 200)}
                     className="snakeButton mr-1"
                   >
                     EASY
                   </span>
                   <span
-                    // onClick={this.startGame.bind(this, 120)}
+                    onClick={this.startGame.bind(this, 120)}
                     className="snakeButton mr-1 ml-1"
                   >
                     MEDIUM
                   </span>
                   <span
-                    // onClick={this.startGame.bind(this, 75)}
+                    onClick={this.startGame.bind(this, 75)}
                     className="snakeButton ml-1"
                   >
                     HARD
@@ -132,7 +480,7 @@ class App extends Component {
                 <div className="game-over-text">{this.state.gameOverMsg}</div>
                 <div className="flex mt-2">
                   <span
-                    // onClick={this.endGame}
+                    onClick={this.endGame}
                     className="snakeButton mr-1 ml-1"
                   >
                     RESTART
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,12 +1,35 @@
-import React, { memo } from "react";
+import React from "react";
+import BoardCell from "../BoardCell/BoardCell";
 
-import BoardCell from "components/BoardCell/BoardCell";
 const GameBoard = (props) => {
+  let board = [];
+  let boardWidth = props.boardWidth;
+  let boardHeight = props.boardHeight;
 
-  let board = []
-  let boardWidth = props.boardWidth
-  let boardHeight = props.boardHeight
-  let foodCoordinate = props.foodCoordinate
+  let snake = props.snake;
+  let foodCoordinate = props.foodCoordinate;
+
+  for (let i = 0; i < boardHeight; i++) {
+    let row = [];
+
+    for (let j = 0; j < boardWidth; j++) {
+      let key = `${i}-${j}`; // Create a unique key using row and column indices
+
+      if (snake.isHead([i, j])) {
+        row.push(<BoardCell key={key} cellType="head" />);
+      } else if (snake.isOn([i, j])) {
+        row.push(<BoardCell key={key} cellType="body" />);
+      } else if (foodCoordinate[0] === i && foodCoordinate[1] === j) {
+        row.push(
+          <BoardCell key={key} foodClass={props.foodClass} cellType="food" />
+        );
+      } else {
+        row.push(<BoardCell key={key} cellType="none" />);
+      }
+    }
+
+    board.push(row);
+  }
   return (
     <div className="flex">
       <div className="game-board">{board}</div>
@@ -14,4 +37,4 @@ const GameBoard = (props) => {
   );
 };
 
-export default memo(GameBoard);
+export default GameBoard;
